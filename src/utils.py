97cd8ffbe22fe1e3f58c7efe27daf31254d8b817
Mohamed Dwedar <mdwedar@revic.ai>
2025-09-22 18:08:38 +0300

    move data utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,14 @@
 import torch
+import os
+import shutil
+from tqdm import tqdm
 
 def nmse(pred, target):
     return torch.sum((pred - target) ** 2) / torch.sum(target ** 2)
+
+def move_data(files, src_dir, dest_dir):
+    
+    for filename in tqdm(files, desc=f"Moving files from {src_dir} to {dest_dir}"):
+        source = os.path.join(src_dir, filename)
+        dest = os.path.join(dest_dir, filename)
+        shutil.move(source, dest)
